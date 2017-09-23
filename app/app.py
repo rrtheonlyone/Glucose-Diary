9e6c13ac8140e9b5d446e470a91a8333992d9dcb
Rahul Rajesh <rahul.rajesh@hotmail.com>
2017-09-23 10:28:29 +0800

    Uploaded Server

--- app/app.py
+++ app/app.py
@@ -1,20 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
-from flask_sqlalchemy import SQLAlchemy
-
-from models import *
 
 app = Flask(__name__)
 api = Api(app)
 
-app.config.from_object('config')
-db = SQLAlchemy(app)
+class Sorting(Resource):
 
-class GetReadings(Resource):
     def get(self):
-        return {'Reading': '6.4'}
+        return "This is the sorting problem answer"
 
-api.add_resource(GetReadings, '/')
+    def post(self):
+    	raw_list = request.get_data()
+    	print(list(name))
+
+    	return "test"
+
+api.add_resource(Sorting, '/')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
